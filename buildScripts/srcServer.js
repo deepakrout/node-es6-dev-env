72a468a5d8cd022695d162bc1295a24e1bd9c21f
Deepak Kumar Rout <der9078@der9078-087497.home>
2018-07-29 10:51:27 -0400

    First commit after webpack configuration

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,30 @@
+/* var express = require('express'),
+    path = require('path'),
+    open = require('open'); */
+import express from 'express'
+import path from 'path'
+import open from 'open'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../src/index.html'));
+})
+
+app.listen(port,function(err){
+    if(err){
+        console.log(err)
+    }
+    else{
+        open(`http://localhost:${port}`)
+    }
+})
