39a1a93b12830f744ad7a060dd3efeaffb53a9e8
Deepak Kumar Rout <deepakrout@hotmail.com>
2018-08-02 23:00:57 -0400

    Commit after Mocha Ut

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+//This file is not transpiled, so must use CommonJS or ES5
+
+//Register babel to transpile before our tests run
+require('babel-register')()
+
+//Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
